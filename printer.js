09977e164068bdc30854385ae39c7cb546d57699
brain <zhengyong145@163.com>
2018-11-09 16:44:25 +0800

    optimize props
    optimize props

--- printer.js
+++ printer.js
@@ -14,14 +14,14 @@ function printer(obj, level = 2) {
             next.forEach(function (aDescSub) {
                 if (aDescSub && aDescSub.length) {
                     var [currentLevel, currentColumn, subObj, subKey] = aDescSub;
-                    printObj(subObj, currentLevel);
+                    printObj(subObj, currentLevel,subKey);
                 }
 
             });
         }
     }
 
-    function printObj(obj, currentLevel) {
+    function printObj(obj, currentLevel,parentKey) {
         if(!obj){
             return;
         }
@@ -31,7 +31,8 @@ function printer(obj, level = 2) {
         var keys = Object.keys(obj);
         var next=keys.map(function (subKey, currentColumn) {
             var subObj = obj[subKey];
-            var pre2 = pre + currentColumn + "列: ";
+            var pre2 = pre + (currentColumn<10?"0"+currentColumn:currentColumn) + "列:";
+            pre2=currentLevel>1?pre2+"["+(parentKey||"")+"]:":pre2;
             if (subObj instanceof Object) {
                 if (currentLevel <= level) {
                     console.log(pre2, subKey);
